e5c1b72b79898e486d11e842c140b5656b8780f6
ceetoo7 <chaudhary10cn@gmail.com>
2025-06-10 08:40:33 +0545

    first commit

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,9 @@
+const Register = ()=>{
+    return (
+        <form className="register-form">
+            <h1>Register Page</h1>
+            <label htmlFor="username">Username : </label>
+            <input type="text" name="username" />
+        </form>
+    )
+}
